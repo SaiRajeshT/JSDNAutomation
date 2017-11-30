d0f50a08f7c2b3074e90041d55bf65f356a0fec9
PRADEEP1-MK <pradeep.mk9@gmail.com>
2017-11-30 18:36:29 +0530

    Enterprise Admin Login function added

---
+++ src/main/java/com/jamcracker/objectRepository/customer/EnterPriseAdminLoginPage.java
@@ -0,0 +1,25 @@
+package com.jamcracker.objectRepository.customer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.jamcracker.utilities.TestBase;
+
+public class EnterPriseAdminLoginPage extends TestBase {
+	
+	public EnterPriseAdminLoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+
+	@FindBy(id="loginEmail")
+	public WebElement usernameTextBox;
+	
+	@FindBy(id="password")
+	public WebElement passwordTextBox;
+	
+	@FindBy(xpath="//button[@title='login']")
+	public WebElement signInButton;
+	
+}
